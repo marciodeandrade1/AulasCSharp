18953c1fd16cac6ba4cd3be053f1271f14d1d825
Marcio de Andrade <marcio@correnteeletrica.com.br>
2021-12-01 09:58:16 -0400

    variáveis e constantes
    Declaração de variáveis e constantes

---
+++ Aula2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    class Program
+    {
+        private const int V = 2;
+
+        static void Main(string[] args)
+        {
+            //Variáveis são posições na memória
+            //declaração de variáveis
+            //sintaxe tipo nome
+            int x = 1;
+            double y = 10.5;
+            string z = "palavra";
+            char c = 'M';
+
+            var valor = V;
+
+            //constante são posições que não podem mudar
+            
+
+
+        }
+    }
+}
