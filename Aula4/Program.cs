4679420cb756102242ad284d132e224984dc2453
Marcio de Andrade <marcio@correnteeletrica.com.br>
2021-12-01 14:39:17 -0400

    Operação com variáveis
    Exemplo de soma usando variáveis

---
+++ Aula4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração das variáveis do tipo inteiro
+            int a, b, c;
+            //exibe mensagem
+            Console.WriteLine("Digite um número: ");
+            //Leitura do valor formato de texto precisa ser convertido para o mesmo tipo da variável
+            //variável a recebe o valor
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            b = int.Parse(Console.ReadLine());
+            //executa a operação soma dos valores recebidos e armazena na variável c
+            c = a + b;
+            //Exibe resultado da soma
+            Console.WriteLine("A soma é:  " + c);
+            Console.WriteLine("A soma é: " + (a + b));
+            //Aguarda leitura de uma tecla para encerrar
+            Console.ReadKey();
+
+        }
+    }
+}
