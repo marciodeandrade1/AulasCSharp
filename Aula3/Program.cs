325b6f49d8237b837addefcdf43ce5922532fd44
Marcio de Andrade <marcio@correnteeletrica.com.br>
2021-12-01 10:08:18 -0400

    Usando variáveis
    Concatenação de variáveis

---
+++ Aula3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Usando variáveis
+            string nome = "Milena";
+            string sobrenome = "Rufini";
+            //Concatenação e exibição dos valores
+            Console.WriteLine(nome + " " + sobrenome);
+            Console.WriteLine(sobrenome + " " + nome);
+            //aguarda usuário precionar uma tecla para encerrar a rotina
+            Console.ReadKey();
+
+        }
+    }
+}
